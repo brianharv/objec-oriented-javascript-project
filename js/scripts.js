0aec039f5d9b6a49cb2bb45dcc00442d442fd4a5
Brian Harvey <brian.harv3y@gmail.com>
2020-08-28 09:57:41 -0700

    add html forms

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+//Business Logic for Pizza Constructor
+function Pizza(size, toppings) {
+  this.size = size,
+  this.toppings = toppings
+}
+/*
+Pizza.prototype.addToppings = function(toppings) {
+  let toppingsValue = 
+  }
+
+}
+*/
+// Business Logic for Shopping Cart
+function ShoppingCart() {
+  this.pizzas = [];
+  this.pizzaId = 0;
+}
+
+ShoppingCart.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza),
+  this.totalPizzas();
+}
+
+ShoppingCart.prototype.assignPizzaId = function() { //assign Id /giftCount
+  this.pizzaId += 1;
+  return this.pizzaId;
+}
+
+ShoppingCart.prototype.
+
+ShoppingCart.prototype.pizzaTracker = function(id) { //find contact or search
+  for (let i=0; i< this.pizzas.length -1; i++) {
+    if (this.pizzas[i]) {
+      if (this.pizzas[i]. id == id) {
+        return this.pizzas[i];
+      }
+    } 
+  };
+  return false;
+}
+
+// User Interface Logic
